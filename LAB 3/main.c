33171fcd9f39683450a57b722de3169a4423eb11
1BF24CS243 <rakshitakr.cs24@bmsce.ac.in>
2026-03-27 12:36:13 +0530

    Add files via upload

--- LAB 3/main.c
+++ LAB 3/main.c
@@ -1,66 +1,81 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, time = 0, remain;
-    int tq;
+int main(){
 
+    int n;
     printf("Enter number of processes: ");
-    scanf("%d", &n);
+    scanf("%d",&n);
 
-    int at[n], bt[n], rt[n], wt[n], tat[n];
+    int AT[n],BT[n],type[n];
 
-    // Input AT and BT in same line
-    for(i = 0; i < n; i++) {
-        printf("Enter Arrival Time and Burst Time for process %d: ", i + 1);
-        scanf("%d %d", &at[i], &bt[i]);
-        rt[i] = bt[i];
+    printf("Enter AT, BT and Type (0=System, 1=User)\n");
+
+    for(int i=0;i<n;i++){
+        printf("For process %d:\t", i+1);
+        scanf("%d%d%d",&AT[i],&BT[i],&type[i]);
     }
 
-    printf("Enter Time Quantum: ");
-    scanf("%d", &tq);
+    int CT[n],TAT[n],WT[n],done[n];
 
-    remain = n;
+    for(int i=0;i<n;i++)
+        done[i]=0;
 
-    while(remain > 0) {
-        int executed = 0;
+    int t=0,count=0;
 
-        for(i = 0; i < n; i++) {
-            if(at[i] <= time && rt[i] > 0) {
-                executed = 1;
+    while(count<n){
 
-                if(rt[i] > tq) {
-                    time += tq;
-                    rt[i] -= tq;
-                } else {
-                    time += rt[i];
-                    wt[i] = time - at[i] - bt[i];
-                    rt[i] = 0;
-                    remain--;
+        int in=-1;
+        int min_at=9999;
+
+        for(int i=0;i<n;i++){
+            if(done[i]==0 && AT[i]<=t && type[i]==0){
+                if(AT[i]<min_at){
+                    min_at=AT[i];
+                    in=i;
                 }
             }
         }
 
-        if(executed == 0) {
-            time++;
+        if(in==-1){
+            for(int i=0;i<n;i++){
+                if(done[i]==0 && AT[i]<=t && type[i]==1){
+                    if(AT[i]<min_at){
+                        min_at=AT[i];
+                        in=i;
+                    }
+                }
+            }
         }
+
+        if(in==-1){
+            t++;
+            continue;
+        }
+
+        t+=BT[in];
+        CT[in]=t;
+        TAT[in]=CT[in]-AT[in];
+        WT[in]=TAT[in]-BT[in];
+
+        done[in]=1;
+        count++;
     }
 
-    for(i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
+    float avgWT=0,avgTAT=0;
+
+    printf("\nID\tType\tAT\tBT\tCT\tWT\tTAT\n");
+
+    for(int i=0;i<n;i++){
+        printf("%d\t%s\t%d\t%d\t%d\t%d\t%d\n",
+        i+1,type[i]==0?"System":"User",
+        AT[i],BT[i],CT[i],WT[i],TAT[i]);
+
+        avgWT+=WT[i];
+        avgTAT+=TAT[i];
     }
 
-    printf("\nProcess\tAT\tBT\tWT\tTAT\n");
-
-    float total_wt = 0, total_tat = 0;
-
-    for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], wt[i], tat[i]);
-        total_wt += wt[i];
-        total_tat += tat[i];
-    }
-
-    printf("\nAverage Waiting Time = %.2f", total_wt / n);
-    printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
+    printf("\nAverage Waiting Time: %.2f",avgWT/n);
+    printf("\nAverage Turnaround Time: %.2f\n",avgTAT/n);
 
     return 0;
 }
